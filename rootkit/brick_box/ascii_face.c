8af2f8895b1903db826a01e0357c4f78c6edca2c
Baremetal MSI <spark61974@gmail.com>
2024-12-19 15:49:33 -0500

    initial commit, make ascii art face for end of comp break when we brick the boxes and print a funny message

---
+++ rootkit/brick_box/ascii_face.c
@@ -0,0 +1,51 @@
+/* instructions
+
+gcc -o broadcast broadcast.c
+
+./broadcast | wall
+
+*/
+
+
+#include <stdio.h> //allows for the compiled file to used inputs and outputs
+
+int main(void) {
+printf("          .- \"\"\"\"\"\"\"\" -.\n");
+printf("       .'                '.\n");
+printf("     .'                    '.\n");
+printf("    /       \\\\      //       \\\n");
+printf("   |         \\\\    //         |\n");
+printf("   |        ( .)   ( .)       |\n");
+printf("   |                          |\n");
+printf("   |                          |\n");
+printf("   |    \\                     |\n");
+printf("   |     \\  /\\  /\\  /\\  /     |\n");
+printf("    \\     \\/  \\/  \\/  \\/     /\n"); 
+printf("     '.                    .'\n");
+printf("       '.                .'\n");
+printf("         '-.__________.-'\n\n");
+// Print your message
+printf("Behold my evil grin, mortals! Mwahahaha!\n");
+    return 0;
+}
+
+
+/*
+
+          .- """""""" -.
+       .'                '.
+     .'                    '.
+    /       \\      //       \
+   |         \\    //         |
+   |        ( .)   ( .)       |
+   |                          |
+   |                          |
+   |    \                     |
+   |     \  /\  /\  /\  /     |
+    \     \/  \/  \/  \/     / 
+     '.                    .'
+       '.                .'
+         '-.__________.-'
+
+
+*/
