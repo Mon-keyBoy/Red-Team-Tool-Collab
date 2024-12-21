3f49fe0ee18a4dcbc35d711a1b3dce52ad6b897d
Baremetal MSI <spark61974@gmail.com>
2024-12-21 13:36:37 -0500

    added includes

--- rootkit/rootkit_lkm.c
+++ rootkit/rootkit_lkm.c
@@ -1,3 +1,6 @@
+//these files will need to be within the environment that you compile this code 
+//but not within the environment that you execute that compiled file in. 
+
 #include <sys/param.h>
 #include <sys/kernel.h>
 #include <sys/module.h>
